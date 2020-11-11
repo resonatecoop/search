e0e15e582debf5149d28e7367e69c646bfabe5c7
Augustin Godiscal <dev@auggod.io>
2020-11-11 17:11:45 +0100

    enable httpauth for es

--- src/elasticClient.js
+++ src/elasticClient.js
@@ -3,6 +3,7 @@ import es from 'elasticsearch'
 const client = new es.Client({
   host: process.env.ELASTIC_HOST || 'localhost:9200',
   log: process.env.ELASTIC_LOG || 'trace',
+  httpAuth: process.env.ELASTIC_USER + ':' + process.env.ELASTIC_PASSWORD,
   apiVersion: process.env.ELASTIC_VERSION || '7.6'
 })
 
